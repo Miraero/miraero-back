ca2f56da1c491777448010825c657c763cdd11c5
Kani Kim <kkh5428@gmail.com>
2022-10-15 17:27:52 +0900

    test file for github actions and ACR

---
+++ src/main/java/com/example/miraero/MiraeroApplication.java
@@ -0,0 +1,13 @@
+package com.example.miraero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiraeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiraeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/miraero/MiraeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miraero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiraeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
