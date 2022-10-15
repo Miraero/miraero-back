7af162ee2fc30646a558f04f15aac6597fed433e
Kani Kim <kkh5428@gmail.com>
2022-10-15 19:03:12 +0900

    renmae

--- src/main/java/com/example/miraero/MiraeroApplication.java
+++ src/main/java/com/example/miraero/MiraeroApplication.java
@@ -1,4 +1,4 @@
-package com.example.miraero;
+package main.java.com.example.miraero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/miraero/MiraeroApplicationTests.java
+++ src/test/java/com/example/miraero/MiraeroApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.miraero;
+package test.java.com.example.miraero;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
