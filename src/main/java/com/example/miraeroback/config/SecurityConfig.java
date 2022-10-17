68a3800456a2c1f0b101f1041c1167c9ab5268d4
Kani Kim <kkh5428@gmail.com>
2022-10-17 21:38:38 +0900

    Update SecurityConfig.java

--- src/main/java/com/example/miraeroback/config/SecurityConfig.java
+++ src/main/java/com/example/miraeroback/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOriginPattern("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
